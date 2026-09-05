97fce8f0317d5bc5f092445ddb2a8dd17d0b5364
aabannn <abanabdlnzr2525@gmail.com>
2026-09-05 15:32:38 +0530

    Update config.js

--- brain-genius/frontend/js/config.js
+++ brain-genius/frontend/js/config.js
@@ -13,7 +13,7 @@ const SUPABASE_ANON_KEY = "sb_publishable_e95Q9qxcnDAm6q7C3x79PA_k8-7y19c";
 // with it behind the scenes.
 const ADMIN_EMAIL = "abanabdlnzr3@gmail.com";
 
-const TOTAL_ROUNDS = 10;
+const TOTAL_ROUNDS = 30;
 const START_LIVES = 3;
 const ROUND_TIME = 13000; // ms
 const GAME_ORDER = ["constellation", "balls", "web", "shapes", "simon"];
